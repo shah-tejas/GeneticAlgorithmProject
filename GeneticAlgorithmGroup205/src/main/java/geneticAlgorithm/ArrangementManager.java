84fd4f043ecb187dcaa4f2d84dd72786d8ba77f7
Tejas Shah <shah.te@husky.neu.edu>
2018-12-02 23:23:35 -0500

    GeneticAlgorithmGroup205/src/main/java/geneticAlgorithm/Arrangement.java
    Implementing ArrangementManager and Arrangement class with fitness function

--- GeneticAlgorithmGroup205/src/main/java/geneticAlgorithm/ArrangementManager.java
+++ GeneticAlgorithmGroup205/src/main/java/geneticAlgorithm/ArrangementManager.java
@@ -1,5 +1,55 @@
 package geneticAlgorithm;
 
+import java.util.ArrayList;
+
 public class ArrangementManager {
 
+	private static ArrayList<Seat> seats;
+	private static ArrayList<Table> tables;
+	private static ArrayList<Person> persons;
+	
+	public ArrangementManager() {
+		seats = new ArrayList<>();
+		tables = new ArrayList<>();
+		persons = new ArrayList<>();
+	}
+	
+	//methods for seats
+	public static void addSeat(Seat seat) {
+		seats.add(seat);
+	}
+	
+	public static Seat getSeat(int index) {
+		return seats.get(index);
+	}
+	
+	public static int numberOfSeats() {
+		return seats.size();
+	}
+	
+	//methods for tables
+	public static void addTable(Table table) {
+		tables.add(table);
+	}
+	
+	public static Table getTable(int index) {
+		return tables.get(index);
+	}
+	
+	public static int numberOfTables() {
+		return tables.size();
+	}
+	
+	//methods for persons
+	public static void addPerson(Person person) {
+		persons.add(person);
+	}
+	
+	public static Person getPerson(int index) {
+		return persons.get(index);
+	}
+	
+	public static int numberOfPersons() {
+		return persons.size();
+	}
 }
