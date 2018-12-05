085080387efe84fe81721ea63ff5e8391f535a11
Tejas Shah <shah.te@husky.neu.edu>
2018-12-04 21:36:31 -0500

    log4j implementation

---
+++ GeneticAlgorithmGroup205/src/main/java/geneticAlgorithm/GAImplementation.java
@@ -0,0 +1,13 @@
+package geneticAlgorithm;
+
+import org.apache.log4j.Logger;
+
+public class GAImplementation {
+	
+	final static Logger logger = Logger.getLogger(GAImplementation.class);
+
+	public static void main(String args[]) {
+		
+		logger.debug("Debug message");
+	}
+}
